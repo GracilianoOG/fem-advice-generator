563fe31ee9fd9a37ea603e07a38610d5c857533e
Gabriel <gabriel.gracilianomb@gmail.com>
2024-09-10 21:19:24 -0300

    Disable button if fetching

--- src/js/script.js
+++ src/js/script.js
@@ -4,13 +4,19 @@ const cardBtn = document.querySelector(".js-card-button");
 const url = "https://api.adviceslip.com/advice";
 
 const getAdvice = (url) => {
+  cardBtn.disabled = true;
+  cardBtn.classList.add("disabled");
   fetch(url, { cache: "no-cache" })
     .then(response => response.json())
     .then(data => {
       cardId.textContent = data.slip.id;
       cardAdvice.textContent = data.slip.advice;
     })
-    .catch(error => console.error("Fetch error: " + error));
+    .catch(error => console.error("Fetch error: " + error))
+    .finally(() => {
+      cardBtn.disabled = false;
+      cardBtn.classList.remove("disabled");
+    });
 }
 
 cardBtn.addEventListener("click", () => getAdvice(url));
